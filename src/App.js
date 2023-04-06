ec685334ad5b1573b33e36223e6a85731986f936
DRGWeiss <ruizd1710@gmail.com>
2023-04-05 20:57:53 -0400

    navbar and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react'
+
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primeflex/primeflex.css'
+
+import './styles/main.scss'
+
+import NavBar from './components/NavBar'
+
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <>
+      <header>
+        <NavBar />
+      </header>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react'
+
+import { Avatar } from 'primereact/avatar'
+import { Menu } from 'primereact/menu'
+
+function NavBar() {
+  const userMenu = useRef(null)
+  const userMenuItems = [
+    {
+      label: sessionStorage.getItem('username')
+        ? sessionStorage.getItem('display_name')
+        : 'Guest',
+    },
+    {
+      label: sessionStorage.getItem('userid') ? 'Logout' : 'Login',
+    },
+  ]
+  
+  return (
+    <div className="navbar">
+      <h3>Custom Job Tracker</h3>
+      <Avatar
+        icon='pi pi-user'
+        size="large"
+        shape="circle"
+        onClick={(e) => userMenu.current.toggle(e)}
+      />
+      <Menu model={userMenuItems} popup ref={userMenu} />
+    </div>
+  )
+}
+
+export default NavBar
