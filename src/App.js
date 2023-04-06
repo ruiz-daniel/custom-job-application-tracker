d687667d07cb231c6115a4d2998bf7ed2fd4e7f9
DRGWeiss <ruizd1710@gmail.com>
2023-04-06 17:09:12 -0400

    testing components with info

--- src/App.js
+++ src/App.js
@@ -9,15 +9,29 @@ import './styles/main.scss'
 
 import NavBar from './components/NavBar'
 import ApplicationsTable from './components/ApplicationsTable'
+import ApplicationCard from './components/ApplicationCard'
+
+import { applicationApi } from './services/api'
 
 function App() {
+  const [applications, setApplications] = React.useState([])
+  React.useEffect(() => {
+    applicationApi.get('642d542000645d89554932ce', (response) =>
+      setApplications(response.data),
+    )
+  })
   return (
     <>
       <header>
         <NavBar />
       </header>
-      <section className='main-section'>
-        <ApplicationsTable />
+      <section className="main-section">
+        <ApplicationsTable applications={applications} />
+      </section>
+      <section className="main-section-card">
+        {applications.map((application) => 
+          <ApplicationCard application={application} />
+        )}
       </section>
     </>
   )
